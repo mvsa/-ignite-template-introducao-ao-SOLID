2359d027ae648c82d3a1d5c698aae56faa633351
Arcanjo <marcos.silva.arcanjo2@gmail.com>
2021-11-18 17:39:57 -0300

    solution implemented

--- src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersUseCase.ts
@@ -1,15 +1,21 @@
-import { User } from "../../model/User";
-import { IUsersRepository } from "../../repositories/IUsersRepository";
+import { User } from '../../model/User';
+import { IUsersRepository } from '../../repositories/IUsersRepository';
 
 interface IRequest {
-  user_id: string;
+  user_id: any;
 }
 
 class ListAllUsersUseCase {
   constructor(private usersRepository: IUsersRepository) {}
 
   execute({ user_id }: IRequest): User[] {
-    // Complete aqui
+    const user = this.usersRepository.findById(user_id);
+
+    if (!user.admin || !user) {
+      throw new Error('Not authorized');
+    }
+
+    return this.usersRepository.list();
   }
 }
 
--- src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
+++ src/modules/users/useCases/turnUserAdmin/TurnUserAdminController.ts
@@ -1,12 +1,18 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 
-import { TurnUserAdminUseCase } from "./TurnUserAdminUseCase";
+import { TurnUserAdminUseCase } from './TurnUserAdminUseCase';
 
 class TurnUserAdminController {
   constructor(private turnUserAdminUseCase: TurnUserAdminUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.params;
+      const user = this.turnUserAdminUseCase.execute({ user_id });
+      return response.json(user).status(201);
+    } catch (err) {
+      return response.status(404).json({ error: err.message });
+    }
   }
 }
 
